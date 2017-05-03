767c628e3520d03021264ff3024fc9eee0adb4e7
Elochai <buu9992@gmail.com>
2017-05-03 13:05:12 +0300

    IFT Custom integration
    https://trello.com/c/1PrkmRC4/

---
+++ lib/omniauth-ift-custom.rb
@@ -0,0 +1,8 @@
+require "omniauth-ift-custom/version"
+require 'omniauth/strategies/ift_custom'
+
+module Omniauth
+  module IftCustom
+
+  end
+end
---
+++ lib/omniauth/strategies/ift_custom.rb
@@ -0,0 +1,53 @@
+require 'omniauth-oauth2'
+require 'builder'
+
+module OmniAuth
+  module Strategies
+    class IftCustom < OmniAuth::Strategies::OAuth2
+      option :name, 'ift_custom'
+
+      option :client_options, { login_page_url: 'MUST BE PROVIDED' }
+
+      uid { info[:uid] }
+
+      info { raw_user_info }
+
+      def request_phase
+        redirect login_page_url
+      end
+
+      def callback_phase
+        self.env['omniauth.auth'] = auth_hash
+        self.env['omniauth.origin'] = '/' + request.params['slug']
+        self.env['omniauth.redirect_url'] = request.params['redirect_url'].presence
+        call_app!
+      end
+
+      def auth_hash
+        hash = AuthHash.new(provider: name, uid: uid)
+        hash.info = info
+        hash
+      end
+
+      def raw_user_info
+        {
+          uid: request.params['uid'],
+          first_name: request.params['first_name'],
+          last_name: request.params['last_name'],
+          email: request.params['email'],
+          username: request.params['username'],
+          is_member: request.params['is_member'],
+          member_type: request.params['member_type'],
+          external_product_ids: request.params['external_product_ids']
+        }
+      end
+
+      private
+
+      def login_page_url
+        options.client_options.login_page_url
+      end
+
+    end
+  end
+end
